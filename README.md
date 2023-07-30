782858783e67d6e4a76f6f00795e8cba92df598b
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-30 18:17:29 +0530

    Add: README.md for usage, contract update in main.js

---
+++ README.md
@@ -0,0 +1,8 @@
+To run the game 
+1. Start the server using
+  - node main.js
+
+2. Add client and pipe output stream to assistant.js
+  - telnet 127.0.0.1 8080 | node assistant.js 
+
+3. Type the number suggested by assistant and press enter
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { GuessGame } = require("./guess-game");
 const net = require("node:net");
 const generateRandomNumber = () => Math.ceil((Math.random() * 1000) % 1024);
 
-const gameServer = (randomNumber, chances, server) => {
+const gameServer = (chances, server) => {
   server.listen(8080, () => {
     console.log("server stated listenting");
   });
@@ -11,13 +11,18 @@ const gameServer = (randomNumber, chances, server) => {
   server.on("connection", (socket) => {
     console.log("New Player joined");
     socket.setEncoding("utf-8");
+
+    const randomNumber = generateRandomNumber();
+    console.log(randomNumber);
     const guessGame = new GuessGame(randomNumber, chances);
     const guessController = new GuessController(guessGame);
 
     socket.on("data", (data) => {
       const number = Number(data.trim());
+      console.log(number);
       const message = guessController.guessAndFeedBack(number);
-      socket.write(`${message}\n`);
+      socket.write(JSON.stringify(message) + '\n');
+
       if (guessController.isGameOver()) {
         socket.end();
       }
@@ -26,12 +31,10 @@ const gameServer = (randomNumber, chances, server) => {
 };
 
 const main = () => {
-  const randomNumber = generateRandomNumber();
-  console.log(randomNumber);
-  const chances = 5;
+  const chances = 25;
   const server = net.createServer();
 
-  gameServer(randomNumber, chances, server);
+  gameServer(chances, server);
 };
 
 main();
